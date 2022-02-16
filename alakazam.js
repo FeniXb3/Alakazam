694b806ce3b449e14aac2069241bf6120d2bc296
Konrad Gadzina <konrad@makegames.today>
2022-02-16 23:50:43 +0100

    Add nodes on click

--- alakazam.js
+++ alakazam.js
@@ -1,17 +1,25 @@
 export class Flowchart {
     constructor() {
         this.entryNode = null;
+        this.nodes = [];
+    }
+
+    addNode(description) {
+        const newNode = new Node(description);
+        this.nodes.push(newNode);
+
+        return newNode;
     }
     
     prepare() {
-        const firstNode = new Node("First node");
-        const secondNode = new Node("Second node");
-        const thirdNode = new Node("Third node");
-        const fourthNode = new Node("Fourth node");
+        const firstNode = this.addNode("First node");
+        const secondNode = this.addNode("Second node");
+        const thirdNode = this.addNode("Third node");
+        const fourthNode = this.addNode("Fourth node");
 
         this.entryNode = firstNode;
 
-        firstNode.connect(secondNode);
+        firstNode.connect(secondNode, "Description");
         firstNode.connect(thirdNode);
         secondNode.connect(thirdNode);
         thirdNode.connect(fourthNode);
@@ -19,8 +27,41 @@ export class Flowchart {
     }
 
     generateCode() {
-        return `graph TD
+        const code =  `graph TD
         ${this.entryNode.getConnectionText()}`;
+
+        this.nodes.forEach(n => n.dirty = false);
+
+        return code;
+    }
+
+    addNodeTo(mermaidNodeId, shouldReattachConnected, nodeDescription, connectionDescription) {
+        const currentNode = this.findNodeByMermaidId(mermaidNodeId);
+        console.log(currentNode);
+        if (!currentNode) {
+            return;
+        }
+
+        const newNode = this.addNode(nodeDescription);
+
+        if (shouldReattachConnected) {
+            currentNode.connections.forEach(c => {
+                newNode.connect(c.target, c.description);
+            });
+
+            currentNode.connections = [];
+        }
+
+        currentNode.connect(newNode, connectionDescription);
+    }
+
+    findNodeByMermaidId(mermaidId) {
+        const pattern = /^flowchart-(\w+)-/gm;
+        const matches = pattern.exec(mermaidId);
+        console.log(matches);
+        const nodeId = matches[1];
+        console.log(nodeId);
+        return this.nodes.find(n => n.id == nodeId);
     }
 }
 
@@ -68,6 +109,12 @@ class Connection {
     }
 
     getConnectionText() {
-        return `-->  ${this.target.getNodeText()}\n${this.target.getConnectionText()}`;
+        return `${this.getArrowText()} ${this.target.getNodeText()}\n${this.target.getConnectionText()}`;
+    }
+
+    getArrowText() {
+        return this.description
+            ? `-->|${this.description}|`
+            : `-->`;
     }
 }
