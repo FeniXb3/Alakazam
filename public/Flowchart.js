81824f381a714c10d7d34d740440be302f28b76c
Konrad Gadzina <konrad@makegames.today>
2022-10-19 21:22:59 +0200

    Change logical operators in example to C# style

--- public/Flowchart.js
+++ public/Flowchart.js
@@ -38,7 +38,7 @@ export class Flowchart {
         const nameDisplayingNode = this.addNode("Player 2 won!", "output");
         const championDisplayinNode = this.addNode("Player 1 won!", "output");
         // const decisionNode = this.addNode("(%sign1% == rock AND %sign2% == scissors) OR (%sign1% == scissors AND %sign2% == paper) OR (%sign1% == paper AND %sign2% == rock)", "decision");
-        const decisionNode = this.addNode("(sign1 == \"rock\" AND sign2 == \"scissors\") OR (sign1 == \"scissors\" AND sign2 == \"paper\") OR (sign1 == \"paper\" AND sign2 == \"rock\")", "decision");
+        const decisionNode = this.addNode("(sign1 == \"rock\" && sign2 == \"scissors\") || (sign1 == \"scissors\" && sign2 == \"paper\") || (sign1 == \"paper\" && sign2 == \"rock\")", "decision");
 
         firstNode.connect(sign1GettingNode);
         sign1GettingNode.connect(sign2GettingNode);
