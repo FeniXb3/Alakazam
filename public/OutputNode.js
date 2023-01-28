7151435a9fcf2ac3ef7b5f9197135cbb877aeb8b
Konrad Gadzina <konrad@makegames.today>
2023-01-28 06:45:26 +0100

    Highlight output node

--- public/OutputNode.js
+++ public/OutputNode.js
@@ -3,14 +3,21 @@ import { modal, outputModal } from "./ModalHandler.js";
 import { Alakazam } from "./Alakazam.js";
 
 export class OutputNode extends Node {
+    constructor(expression, type) {
+        const description =  OutputNode.getDescription(expression);
+        super(description, type);
+
+        this.expression = expression;
+    }
+
     perform(state, nextConnection) {
         const operationPattern = /(?:{([^{}]+)})/gm;
-        let parsedText = this.description;
+        let parsedText = this.expression;
         if (parsedText.startsWith('$')) {
             parsedText = parsedText.substring(1);
             let match;
             do {
-                match = operationPattern.exec(this.description);
+                match = operationPattern.exec(parsedText);
                 if (match) {
                     console.log('Parsing ' + match[0]);
                     parsedText = parsedText.replace(match[0], this.getPartialResult(state, match[1]));
@@ -29,5 +36,24 @@ export class OutputNode extends Node {
 
         // alert(parsedText);
         // super.perform(state, nextConnection);
+    }
+
+    getEditInfo() {
+        return {
+            title: 'Update node\'s expression',
+            content: this.expression
+        }
+    }
+
+    update(expression) {
+        this.expression = expression;
+        this.description = OutputNode.getDescription(expression);
+    }
+    refreshDescription() {
+        this.description = OutputNode.getDescription(this.expression)
+    }
+
+    static getDescription(expression) {
+        return `<pre>${Node.getHighlightTagOpening()}${expression}${Node.getHighlightTagClosing()}</pre>`;
     }
 }
